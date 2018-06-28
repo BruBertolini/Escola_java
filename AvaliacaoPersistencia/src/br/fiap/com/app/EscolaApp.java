e677a627e56979620ce1c4bf284cdde54c57e59f
logonrm <logonrm@fiap.com.br>
2018-06-27 21:16:15 -0300

    Correção Database

---
+++ AvaliacaoPersistencia/src/br/fiap/com/app/EscolaApp.java
@@ -0,0 +1,41 @@
+package br.fiap.com.app;
+
+import javax.swing.JOptionPane;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import br.com.fiap.entidades.Escola;
+import br.com.fiap.jdbc.JdbcEscolaDao;
+
+public class EscolaApp {
+	
+	public static void main(String[] args) {
+		
+		 incluirEscola();
+	}
+		
+	private static void incluirEscola() {
+		try {
+			ApplicationContext context = new ClassPathXmlApplicationContext("beansJdbc.xml");
+			JdbcEscolaDao dao = (JdbcEscolaDao) context.getBean("jdbcEscolaDao");
+
+			Escola escola = new Escola();
+			escola.setNome(JOptionPane.showInputDialog("Nome da escola: "));
+			escola.setEndereco(JOptionPane.showInputDialog("Endereço: "));
+			escola.setTelefone(JOptionPane.showInputDialog("Telefone: "));
+			escola.setEmail(JOptionPane.showInputDialog("Email: "));
+
+			dao.incluirEscola(escola);
+			JOptionPane.showMessageDialog(null, "Escola incluída com sucesso");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	
+
+	
+}
+	
---
+++ dbescola.sql
@@ -0,0 +1,100 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: dbescola
+-- ------------------------------------------------------
+-- Server version	5.7.20-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `alunos`
+--
+
+DROP TABLE IF EXISTS `alunos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `alunos` (
+  `idAlunos` int(11) NOT NULL AUTO_INCREMENT,
+  `Nome` varchar(45) DEFAULT NULL,
+  `Email` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idAlunos`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `cursos`
+--
+
+DROP TABLE IF EXISTS `cursos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cursos` (
+  `idCursos` int(11) NOT NULL AUTO_INCREMENT,
+  `Nome` varchar(45) DEFAULT NULL,
+  `Descrição` varchar(45) DEFAULT NULL,
+  `Escola_id` int(11) NOT NULL,
+  `Inicio` date DEFAULT NULL,
+  `Termino` date DEFAULT NULL,
+  `Periodo` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idCursos`,`Escola_id`),
+  KEY `fk_Cursos_Escola_idx` (`Escola_id`),
+  CONSTRAINT `fk_Cursos_Escola` FOREIGN KEY (`Escola_id`) REFERENCES `escola` (`idEscola`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `escola`
+--
+
+DROP TABLE IF EXISTS `escola`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `escola` (
+  `idEscola` int(11) NOT NULL AUTO_INCREMENT,
+  `Nome` varchar(45) DEFAULT NULL,
+  `Endereco` varchar(45) DEFAULT NULL,
+  `Telefone` varchar(45) DEFAULT NULL,
+  `Email` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idEscola`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `nota`
+--
+
+DROP TABLE IF EXISTS `nota`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `nota` (
+  `idNota` int(11) NOT NULL AUTO_INCREMENT,
+  `Alunos_id` int(11) NOT NULL,
+  `Cursos_id` int(11) NOT NULL,
+  `Nota` double DEFAULT NULL,
+  PRIMARY KEY (`idNota`,`Alunos_id`,`Cursos_id`),
+  KEY `fk_Nota_Alunos1_idx` (`Alunos_id`),
+  KEY `fk_Nota_Cursos1_idx` (`Cursos_id`),
+  CONSTRAINT `fk_Nota_Alunos1` FOREIGN KEY (`Alunos_id`) REFERENCES `alunos` (`idAlunos`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Nota_Cursos1` FOREIGN KEY (`Cursos_id`) REFERENCES `cursos` (`idCursos`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-06-27 19:11:36
